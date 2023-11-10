cdd0936f921b37048198ca8909983d4c66049ba9
Camilo Andres Garcia Arias <camilo.garcia.arias@gmail.com>
2023-11-09 20:00:55 -0500

    Creation of migrations, models, drivers, controllers, seeders, factories, policies, update and storage requests

---
+++ app/Http/Controllers/ProductPhotoController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product_photo;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProduct_photoRequest;
+use App\Http\Requests\UpdateProduct_photoRequest;
+
+class ProductPhotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProduct_photoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product_photo $product_photo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product_photo $product_photo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProduct_photoRequest $request, Product_photo $product_photo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product_photo $product_photo)
+    {
+        //
+    }
+}
